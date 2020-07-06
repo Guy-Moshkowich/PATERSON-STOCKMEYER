c1b3da9eb3d58311d2d890396cee30750004b759
guym <guy.moshkowich@ibm.com>
2020-07-06 12:31:34 +0300

    changed order of methods for clarity

--- paterson_stockmeyer_algo.py
+++ paterson_stockmeyer_algo.py
@@ -6,6 +6,38 @@ import numpy as np
  
 '''
 
+def ps(f, u):
+    k, p, m = calc_k_p_m(f)
+    f_tilde = calc_f_tilde(f, k, p)
+    f_tilde_val_in_u = ps_recursive(f_tilde, u, k, p, precomputed_u_powers_less_than_k(u, k), precomputed_u_two_powers(u, k, p))
+    f_val_in_u = f_tilde_val_in_u - u**(k*(2*p - 1))
+    return f_val_in_u
+
+
+def ps_recursive(f, u, k, p, precomputed_u_powers_less_than_k, precomputed_u_two_powers):
+    '''
+    p(x) = [x**(kp)+c(x)]q(x) + [x**(k(p-1)) + s(x)]
+    deg(q(x)) = k(p-1)
+    deg([x**(k(p-1)) + s(x)]) = k(p-1)
+    deg(c(x)) <= k-1 i.e., c(x) can be evaluated
+    x**(kp) can be evaluated
+    '''
+
+    n = deg(f)
+    if n <= k:
+        return evaluate_deg_less_than_k(f, u, k, p, precomputed_u_powers_less_than_k)
+    q, r = calc_q_r(f, k, p)
+    c, s = calc_c_s(r, q, k, p)
+    c_val = evaluate_deg_less_than_k(c, u, k, p, precomputed_u_powers_less_than_k)
+    q_coeff = c_val + precomputed_u_two_powers[int(math.log2(p))]
+    x_power_kp_minus_one = np.append([1], np.zeros(k*(p-1)))
+    s_plus_x_power_kp_minus_one = np.polyadd(x_power_kp_minus_one,s)
+    assert deg(q) == k*(p-1)
+    assert deg(s_plus_x_power_kp_minus_one) == k*(p-1)
+    q_val = ps_recursive(q, u, k, p//2, precomputed_u_powers_less_than_k, precomputed_u_two_powers)
+    s_plus_x_power_kp_minus_one_val = ps_recursive(s_plus_x_power_kp_minus_one, u, k, p // 2, precomputed_u_powers_less_than_k, precomputed_u_two_powers)
+    return q_coeff*q_val + s_plus_x_power_kp_minus_one_val
+
 
 def calc_q_r(f, k, p):
     x_to_the_power_of_kp = np.append([1], np.zeros(k*p))
@@ -75,37 +107,6 @@ def precomputed_u_two_powers(u, k, p):
     return results
 
 
-def ps(f, u):
-    k, p, m = calc_k_p_m(f)
-    f_tilde = calc_f_tilde(f, k, p)
-    f_tilde_val_in_u = ps_recursive(f_tilde, u, k, p, precomputed_u_powers_less_than_k(u, k), precomputed_u_two_powers(u, k, p))
-    f_val_in_u = f_tilde_val_in_u - u**(k*(2*p - 1))
-    return f_val_in_u
-
-
-def ps_recursive(f, u, k, p, precomputed_u_powers_less_than_k, precomputed_u_two_powers):
-    '''
-    p(x) = [x**(kp)+c(x)]q(x) + [x**(k(p-1)) + s(x)]
-    deg(q(x)) = k(p-1)
-    deg([x**(k(p-1)) + s(x)]) = k(p-1)
-    deg(c(x)) <= k-1 i.e., c(x) can be evaluated
-    x**(kp) can be evaluated
-    '''
-
-    n = deg(f)
-    if n <= k:
-        return evaluate_deg_less_than_k(f, u, k, p, precomputed_u_powers_less_than_k)
-    q, r = calc_q_r(f, k, p)
-    c, s = calc_c_s(r, q, k, p)
-    c_val = evaluate_deg_less_than_k(c, u, k, p, precomputed_u_powers_less_than_k)
-    q_coeff = c_val + precomputed_u_two_powers[int(math.log2(p))]
-    x_power_kp_minus_one = np.append([1], np.zeros(k*(p-1)))
-    s_plus_x_power_kp_minus_one = np.polyadd(x_power_kp_minus_one,s)
-    assert deg(q) == k*(p-1)
-    assert deg(s_plus_x_power_kp_minus_one) == k*(p-1)
-    q_val = ps_recursive(q, u, k, p//2, precomputed_u_powers_less_than_k, precomputed_u_two_powers)
-    s_plus_x_power_kp_minus_one_val = ps_recursive(s_plus_x_power_kp_minus_one, u, k, p // 2, precomputed_u_powers_less_than_k, precomputed_u_two_powers)
-    return q_coeff*q_val + s_plus_x_power_kp_minus_one_val
 
 
 if __name__ == '__main__':
