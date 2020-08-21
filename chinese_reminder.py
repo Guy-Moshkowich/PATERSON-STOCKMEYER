7d9bb3f9ccdf4827c5ea5955e7d01309bf30a354
Guy-Moshkowich <guy.moshkowich@gmail.com>
2020-08-21 15:03:00 +0300

    find reminder by iterating over all values in new module range

--- chinese_reminder.py
+++ chinese_reminder.py
@@ -1,5 +1,6 @@
 import numpy
 
+
 def gcdExtended(a, b):
     # Base Case
     if a == 0:
@@ -15,27 +16,31 @@ def gcdExtended(a, b):
     return gcd, x, y
 
 
-# Driver code
-# a, b = 35, 15
-# g, x, y = gcdExtended(a, b)
-# print("gcd(", a, ",", b, ") = ", g)
-# print(x, '*' , a, ' + ', y, '*', b,' = ', g)
+def generate_int(a_param, n_param):
+    N = [numpy.prod(n_param)//n_i for n_i in n_param]
+    M = []
+    for i in range(len(n_param)):
+        g, M_i, m_i = gcdExtended(N[i], n_param[i])
+        M.append(M_i)
+    x = numpy.sum([a_i * N_i * M_i for a_i, N_i, M_i in zip(a_param, N, M)])
+    return x
 
-n = [2, 5, 7, 13, 17]
-N_prod = numpy.prod(n)
-print('N_prod=', N_prod)
-N = [int(N_prod/n_i) for n_i in n]
-M = []
-for i in range(len(n)):
-    g, M_i, m_i = gcdExtended(N[i], n[i])
-    print(M_i,'*',N[i],'+', m_i,'*',n[i],'=1')
-    M.append(M_i)
-print('M_i =', M)
-print('N_i =', N)
-residues = [1, 0, 6, 5]
-print('residues= ', residues)
-num_of_res = len(residues)
-products = [a * N_i * M_i for a, N_i, M_i in zip(residues, N[:num_of_res], M[:num_of_res])]
-sum = numpy.sum(products)
-print('sum =', sum)
-print ('calc_new_residues =', [sum % n_i for n_i in n[:num_of_res]])
+
+n = [2, 5, 7, 13]
+print('n =', n)
+n_new = 17
+print('n_new =', n_new)
+a = [1, 0, 6, 5]
+print('a =', a)
+x_base = generate_int(a, n)
+print('x_base =', x_base)
+n_tag = n.copy()
+n_tag.append(n_new)
+for a_new in range(n_new):
+    a_tag = a.copy()
+    a_tag.append(a_new)
+    x_new = generate_int(a_tag, n_tag)
+    if x_new % numpy.prod(n_tag) == x_base:
+        print('a_new = ', a_new)
+        break
+
